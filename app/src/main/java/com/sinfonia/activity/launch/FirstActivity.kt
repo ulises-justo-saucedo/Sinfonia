5eaa855e384a8b7f25ead828f04cfe33befa8831
Ulises Justo Saucedo <ulisesjustosaucedo@gmail.com>
2024-02-27 06:16:31 -0300

    Now the app asks for permissions before reading user files

--- app/src/main/java/com/sinfonia/activity/launch/FirstActivity.kt
+++ app/src/main/java/com/sinfonia/activity/launch/FirstActivity.kt
@@ -1,6 +1,7 @@
 package com.sinfonia.activity.launch
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -9,6 +10,7 @@ import com.sinfonia.activity.hideSystemUI
 import com.sinfonia.activity.main.MainActivity
 
 class FirstActivity : AppCompatActivity() {
+    private val READ_EXTERNAL_STORAGE_REQUEST_CODE = 1
     private lateinit var btnStart: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,7 +24,11 @@ class FirstActivity : AppCompatActivity() {
     }
     private fun initListeners(){
         btnStart.setOnClickListener {
-            startActivity(Intent(this, MainActivity::class.java))
+            if(checkSelfPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+                requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),READ_EXTERNAL_STORAGE_REQUEST_CODE)
+            }else{
+                startActivity(Intent(this, MainActivity::class.java))
+            }
         }
     }
 }
